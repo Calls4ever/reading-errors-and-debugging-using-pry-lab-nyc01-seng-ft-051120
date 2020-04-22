f3bad3594c8ff2fc9573b4d0ef9ded0f7a07b8e3
Fnu Tsering Norbu <thetseringnorbu@gmail.com>
2020-04-22 02:46:42 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,10 +1,12 @@
 # don't forget to add: require 'pry'
 require 'pry'
 def start_game(player1, player2)
-  "Hello #{player1} & #{playar2}"
+  msg1="Hello #{player1} & #{playar2}"
+  puts msg1
 end
 
 def play_game(player2)
-  msg="#{player1} is better than #{player2}"
+  msg2 ="#{player1} is better than #{player2}"
+  puts msg2
   binding.pry
 end
