cb8a9630508991c8021f8199431f6b52e2d25705
Fnu Tsering Norbu <thetseringnorbu@gmail.com>
2020-04-22 03:18:46 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -3,14 +3,12 @@ require'pry'
 def generate_star_date
   star_date = (rand(100000) + 400000) / 10.0
   p star_date
-  binding.pry
 end
 
 
 def state_log(star_date)
   msg1="Captain's Log, star date #{star_date}."
   p msg1
-  binding.pry
 end
 
 def engage
