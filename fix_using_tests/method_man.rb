9f07cef4b6a144396004b53e9494cbb36ab63774
Fnu Tsering Norbu <thetseringnorbu@gmail.com>
2020-04-22 02:47:32 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -5,7 +5,7 @@ def start_game(player1, player2)
   puts msg1
 end
 
-def play_game(player2)
+def play_game(player1, player2)
   msg2 ="#{player1} is better than #{player2}"
   puts msg2
   binding.pry
